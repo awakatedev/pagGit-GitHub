2ba00284263d30192ed1b73e949428df3422e748
jhonathan <jhonathanjhs10@gmail.com>
2020-12-29 00:41:27 -0400

    right now the search engine, the menu and the carousel work. functionality and styles need to be improved

--- document.js
+++ document.js
@@ -1,11 +1,38 @@
 //-------------MENU-------------
 
-document.getElementById("icon-menu").addEventListener("click", show_menu);
-//vars
-menu_block = document.getElementById("show-menu");
+document.getElementById("menu_icon").addEventListener("click", show_menu);
+document
+  .getElementById("close-menu_icon")
+  .addEventListener("click", close_menu);
+document.getElementById("cover-ctn-menu").addEventListener("click", close_menu);
 
 function show_menu() {
+  let menu_block = document.getElementById("show-menu");
+  let icon_close_menu = document.getElementById("close-menu_icon");
+  let icon_menu = document.getElementById("menu_icon");
+  let cover_ctn_menu = document.getElementById("cover-ctn-menu");
+
   menu_block.style.display = "block";
+  cover_ctn_menu.style.display = "block";
+
+  if ((menu_block.style.display = "block")) {
+    icon_menu.style.display = "none";
+    icon_close_menu.style.display = "block";
+  }
+}
+
+function close_menu() {
+  let menu_block = document.getElementById("show-menu");
+  let icon_close_menu = document.getElementById("close-menu_icon");
+  let icon_menu = document.getElementById("menu_icon");
+  let cover_ctn_menu = document.getElementById("cover-ctn-menu");
+
+  if ((menu_block.style.display = "block")) {
+    menu_block.style.display = "none";
+    cover_ctn_menu.style.display = "none";
+    icon_close_menu.style.display = "none";
+    icon_menu.style.display = "block";
+  }
 }
 
 //-----------BROWSER---------------
