bc9287aa6af553c178d7d97f130e85b2a7561f9f
jhonathan <jhonathanjhs10@gmail.com>
2021-01-28 20:27:45 -0400

    changin many styles, commit after to merge

--- src/options.js
+++ src/options.js
@@ -3,7 +3,7 @@ const menu_block = document.getElementById("show-menu");
 const icon_close_menu = document.getElementById("close-menu_icon");
 const icon_menu = document.getElementById("menu_icon");
 
-document.getElementById("menu_icon").addEventListener("click", function() {
+icon_menu.addEventListener("click", function() {
 
   menu_block.style.display = "block";
   cover_ctn_carousel.style.display = "block";
@@ -13,11 +13,9 @@ document.getElementById("menu_icon").addEventListener("click", function() {
     icon_close_menu.style.display = "block";
   }
 });
-document
-  .getElementById("close-menu_icon")
-  .addEventListener("click", close_menu);
-document.getElementById("cover-ctn-slides").addEventListener("click", close_menu);
 
+icon_close_menu.addEventListener("click", close_menu);
+document.getElementById('cover-ctn-slides').addEventListener("click", close_menu);
 
 function close_menu() {
 
@@ -28,5 +26,15 @@ function close_menu() {
     icon_menu.style.display = "block";
   }
 }
-
 //-_-_-_-_-_-_-###########-_-_-_-_-_-_-_-_-_
+const show_telegram = document.getElementById("show-my-telegram");
+const button_telegram = document.getElementById("icon-show_telegram");
+
+const show_Email = document.getElementById("show-my-email");
+const button_Email = document.getElementById("icon-show_Email");
+
+button_telegram.addEventListener("click", function(){
+show_telegram.style.display = "block"});
+
+button_Email.addEventListener("click", function(){
+show_Email.style.display = "block"});
