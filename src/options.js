e01e38f50f09c9e0419c1fbde2e0d739c87fed86
jhonathan <jhonathanjhs10@gmail.com>
2021-01-30 01:38:57 -0400

    first commit next to merge from styles branch

--- src/options.js
+++ src/options.js
@@ -3,8 +3,7 @@ const menu_block = document.getElementById("show-menu");
 const icon_close_menu = document.getElementById("close-menu_icon");
 const icon_menu = document.getElementById("menu_icon");
 
-icon_menu.addEventListener("click", function() {
-
+icon_menu.addEventListener("click", function () {
   menu_block.style.display = "block";
   cover_ctn_carousel.style.display = "block";
 
@@ -15,10 +14,11 @@ icon_menu.addEventListener("click", function() {
 });
 
 icon_close_menu.addEventListener("click", close_menu);
-document.getElementById('cover-ctn-slides').addEventListener("click", close_menu);
+document
+  .getElementById("cover-ctn-slides")
+  .addEventListener("click", close_menu);
 
 function close_menu() {
-
   if ((menu_block.style.display = "block")) {
     menu_block.style.display = "none";
     cover_ctn_carousel.style.display = "none";
@@ -26,15 +26,3 @@ function close_menu() {
     icon_menu.style.display = "block";
   }
 }
-//-_-_-_-_-_-_-###########-_-_-_-_-_-_-_-_-_
-const show_telegram = document.getElementById("show-my-telegram");
-const button_telegram = document.getElementById("icon-show_telegram");
-
-const show_Email = document.getElementById("show-my-email");
-const button_Email = document.getElementById("icon-show_Email");
-
-button_telegram.addEventListener("click", function(){
-show_telegram.style.display = "block"});
-
-button_Email.addEventListener("click", function(){
-show_Email.style.display = "block"});
